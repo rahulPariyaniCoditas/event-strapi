ab70d40dc520998cbf47b58b6bda84dc384d308f
rahul.pariyani <rahul.pariyani@coditas.com>
2021-03-15 00:52:53 +0530

    make it better

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
       settings: {
         client: "mongo",
         uri:
-          "mongodb://test:test_123@cluster0-shard-00-00.5hgv1.mongodb.net:27017,cluster0-shard-00-01.5hgv1.mongodb.net:27017,cluster0-shard-00-02.5hgv1.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-12dh21-shard-0&authSource=admin&retryWrites=true&w=majority",
+          "mongodb+srv://rahulpariyani:tanmay5@cluster0.5hgv1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
       },
       options: {
         ssl: true,
